96c633dda90813dd471359cd9cea2bd025e8b3d8
gloovers <bcs.andrey@gmail.com>
2018-07-16 09:05:40 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,9 +1,9 @@
 l_str = [int(str) for str in input("Input l=> ").split()]
 max_count = 0
-max = None
+max_v = None
 for element in l_str:
     l_count = l_str.count(element)
     if l_count > max_count:
         max_count = l_count
-        max = element
-print("Max_often =>", max)
+        max_v = element
+print("Max_often =>", max_v)
